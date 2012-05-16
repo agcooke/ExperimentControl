d8f67bdfffc2d48c4882ba5081064bd6081e614f
¨Adrian <Adrian.Cooke@utwente.nl>
2012-05-16 17:11:21 +0200

    The Ant PLus listener is disabled for testing. The socket connection does not work yet.

--- experimentcontrol/bin/experiment-control.py
+++ experimentcontrol/bin/experiment-control.py
@@ -10,21 +10,11 @@ and event handlers.
 """
 import logging
 import optparse
-import sys
 import time
 
-from ant.core import driver
 from ant.core import event
-from ant.core.constants import RESPONSE_NO_ERROR
-from ant.core.message import ChannelAssignMessage
-from ant.core.message import ChannelFrequencyMessage
-from ant.core.message import ChannelIDMessage
-from ant.core.message import ChannelOpenMessage
-from ant.core.message import ChannelPeriodMessage
-from ant.core.message import ChannelSearchTimeoutMessage
-from ant.core.message import NetworkKeyMessage
-from ant.core.message import SystemResetMessage
-from experimentcontrol.core.SofieHdfFormatLogWriter import LogWriter
+from experimentcontrol.core.AntPlusListener import AntPlusListener
+from experimentcontrol.core.InertiaTechnologyListener import IntertiaTechnologyListener
 from experimentcontrol.core.antlogging import setLogger
 NETKEY = '\xB9\xA5\x21\xFB\xBD\x72\xC3\x45'
 
@@ -44,8 +34,14 @@ This package is used log data from the sparkfun usb stick
         default=False,help="Enable verbose mode.")
     parser.add_option('--runname', '-t', default=None,
         help="The Run that you are doing.")
-    parser.add_option('--serial', '-s', default='/dev/ttyUSB0',
+    parser.add_option('--serialant', '-s', default='/dev/ttyUSB0',
         help="Serial Device of the ANT Stick.")
+    parser.add_option('--serialimu', '-i', default='/dev/ttyUSB1',
+        help="Serial Device of the IMU's.")
+    parser.add_option('--imuport', '-p', default=1234,
+        help="The Port for the IMU socket server.")
+    parser.add_option('--imuhost', '-m', default='localhost',
+        help="The host for the IMU socket server.")
     options, arguments = parser.parse_args()
     if options.verbose:
         ch = setLogger(DEBUGLEVEL=logging.DEBUG)
@@ -59,91 +55,21 @@ This package is used log data from the sparkfun usb stick
     if(options.runname==None):
         print "Specify the rest run name (--runname 01CornerTestRun) "
         exit()
-    #Setup the logger:
-    theLogger = LogWriter(filename=options.outfile,runName=options.runname)
-        
-    # Initialize driver
-    stick = driver.USB1Driver(options.serial, log=theLogger,
-        debug=DEBUG,baud_rate=4800)
-    stick.open()
 
-    # Initialize event machine
-    evm = event.EventMachine(stick)
-    evm.registerCallback(MyCallback())
-    evm.start()
-
-    # Reset
-    print "\n\n-------------------------------\n:"
-    print "Setting UP"
-    print "\n\n-------------------------------\n"
-    msg = SystemResetMessage()
-    stick.write(msg.encode())
-    time.sleep(1)
-
-    # Set network key
-    msg = NetworkKeyMessage(key=NETKEY)
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: SETTING NETWORK KEY'
-        sys.exit()
-
-    # Initialize it as a receiving channel using our network key
-    msg = ChannelAssignMessage()
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: INITIALISING AS RECEIVING CHANNEL'
-        sys.exit()
-
-    # Now set the channel id for pairing with an ANT+ bike cadence/speed sensor
-    msg = ChannelIDMessage(device_type=121)
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: SETTING CHANNEL ID '
-        sys.exit()
-
-    # Listen forever and ever (not really, but for a long time)
-    msg = ChannelSearchTimeoutMessage(timeout=255)
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UPL LISTENING TIMEOUT'
-        sys.exit()
-
-    # We want a ~4.05 Hz transmission period
-    msg = ChannelPeriodMessage(period=8085)
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: TRANSMISSION FREQUENCY'
-        sys.exit()
-
-    # And ANT frequency 57, of course
-    msg = ChannelFrequencyMessage(frequency=57)
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: SETTING FREQUENCY'
-        sys.exit()
-
-    # Time to go live
-    msg = ChannelOpenMessage()
-    stick.write(msg.encode())
-    if evm.waitForAck(msg) != RESPONSE_NO_ERROR:
-        print 'ERROR SETTING UP: GOING LIVE'
-        sys.exit()
-        
-    print "\n\n-------------------------------\n:"
-    print "Listening for ANT events: Press CTRL+C to Exit."
-    print "\n\n-------------------------------\n"
+    #antPlusListener = AntPlusListener(options.outfile,
+    #    options.runname,
+    #    options.serialant)
+    intertiaTechnologyListener = IntertiaTechnologyListener(
+        options.outfile,options.runname,
+        options.imuport,options.imuhost,options.serialimu)
+    #antPlusListener.open()
     try:
         while 1:
             time.sleep(100)
     except KeyboardInterrupt:
         pass;
-    print "\n\n-------------------------------\nShutting down:"
-    print "\n\n-------------------------------\n"
-    msg = SystemResetMessage()
-    stick.write(msg.encode())
-    time.sleep(1)
-    evm.stop()
-    stick.close()
+    intertiaTechnologyListener.close()
+    #antPlusListener.close()
     print "\n\n-------------------------------\n:"
     print "EXITING"
     print "\n\n-------------------------------\n"
--- experimentcontrol/core/InertiaTechnologySocketDriver.py
+++ experimentcontrol/core/InertiaTechnologySocketDriver.py
@@ -4,28 +4,24 @@ from experimentcontrol.core.Exceptions import CouldNotConnectToSocket
 class InertiaTechnologySocketDriver (object):
     def __init__(self,host='localhost',port=1234,device='/dev/ttyUSB0',
         mode='+'):
-            '''
+        '''
             Intialise the Driver.
 
             mode= + = autonumber file, a = append file, w = overwrite file
-            '''
+        '''
         self.is_open = False
         self.host = host
-        self.port = port
+        self.port = int(port)
         self.device = device
-        self.open(filename,runName)
-        self.mode = '+'
+        self.mode = mode
+        self.open()
 
     def __del__(self):
-        if self.is_open:
-            self.fd.close()
-            self.fdP.close()
+        self.close()
 
     def open(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.device = device
-        if not self.socket.connect((self.host, self.port)):
-            raise CouldNotConnectToSocket()
+        self.socket.connect((self.host, self.port))
         #if self.is_open == True:
         #    self.close()
         self.is_open = True
@@ -41,19 +37,21 @@ class InertiaTechnologySocketDriver (object):
         self.socket.sendall(command)
 
     def startRecording(self,logFilename):
-        if self.mode = '+':
+        if self.mode == '+':
             existBehaviour = 2;
-        elif self.mode = 'a':
+        elif self.mode == 'a':
             existBehaviour = 1;
         else:
             existBehaviour = 0
-        CMD='open %s -l %s  --existBehaviour %d',(self.device,
+        CMD='open {0} -l {1}  --existBehaviour {2}'.format(self.device,
             logFilename,
             existBehaviour)
+
+        logging.debug(CMD)
         self._writeCommand(CMD)
 
     def stopRecording(self):
-        CMD='close %s',(self.device)
+        CMD='close {0}'.format(self.device)
         self._writeCommand(CMD)
 
 
